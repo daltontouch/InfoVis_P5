6397680f8058ba4670e3279a1258126eccc698b4
Dalton Touchberry <daltontouch@gmail.com>
2019-04-21 07:39:21 -0400

    add new airplane graphic to page

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function start() {
     var littleBuffer = 10;
     var bigBuffer = 40;
 
-    var currentPhase = "STANDING";
+    var currentPhase = 'STANDING';
 
     var svg = d3
         //.select('img/#FlightGraphic')
@@ -21,17 +21,31 @@ function start() {
         .attr('width', width)
         .attr('height', height);
 
-    // var flightChart = d3.xml('img/FlightGraphic.svg', 'image/svg+xml', function(
-    //     xml
-    // ) {
+    // var gButtonBar = d3
+    //     .select(buttonBar)
+    //     .append('svg')
+    //     .attr('width', '1000')
+    //     .attr('height', '600');
+
+    // var flightChart = d3.xml('img/d3.svg', 'image/svg+xml', function(xml) {
     //     var importedNode = document.importNode(xml.documentElement, true);
-    //     d3.select('div#flightChart').each(function() {
+    //     d3.select('div#buttonBar').each(function() {
     //         this.appendChild(importedNode);
     //     });
     // });
 
-    // .then(data => {
-    //     document.body.append(data.documentElement);
+    var FlightGraphic = d3
+        .select('#buttonBar')
+        .append('svg')
+        .attr('src', 'img/FlightGraphic_new.svg')
+        .attr('width', width)
+        .attr('height', width * 0.6);
+
+    // d3.xml('img/FlightGraphic.svg', 'image/svg+xml', function(xml) {
+    //     var importedNode = document.importNode(xml.documentElement, true);
+    //     d3.select('div#buttonBar').each(function() {
+    //         this.appendChild(importedNode);
+    //     });
     // });
 
     //Navigation Buttons
@@ -42,9 +56,18 @@ function start() {
         .append('button')
         .text('No, click me!');
 
+    // var FlightGraphic = gButtonBar
+    //     .append('image')
+    //     .attr('xlink:href', 'img/FlightGraphic.svg')
+    //     .attr('width', 1000)
+    //     .attr('height', 600)
+    //     .then(data => {
+    //         document.body.append(data.documentElement);
+    //     });
+
     var bars = svg.append('g');
 
-    var xScale = d3.scaleBand().rangeRound([0, width - (bigBuffer * 2)], 0.3);
+    var xScale = d3.scaleBand().rangeRound([0, width - bigBuffer * 2], 0.3);
     var yScale = d3.scaleLinear().range([height - bigBuffer, littleBuffer]);
 
     var xAxis = d3.axisBottom(xScale);
@@ -71,101 +94,137 @@ function start() {
             return d;
         },
         function(error, data) {
-            var incidents = d3.nest()
-                .key(function(d) { return d.Broad_Phase_of_Flight; })
+            var incidents = d3
+                .nest()
+                .key(function(d) {
+                    return d.Broad_Phase_of_Flight;
+                })
                 .sortKeys(d3.ascending)
-                .key(function(d) { return d.Make; })
+                .key(function(d) {
+                    return d.Make;
+                })
                 .sortKeys(d3.ascending)
                 //.key(function(d) { return d.Broad_Phase_of_Flight; })
-                .rollup(function(v) { return {
-                    make: v.map(function(d) { return d.Make })[0],
-                    fatal: d3.sum(v, function (d) { return +d.Total_Fatal_Injuries; }),
-                    serious: d3.sum(v, function (d) { return +d.Total_Serious_Injuries; }),
-                    uninjured: d3.sum(v, function (d) { return +d.Total_Uninjured; })
-                 }; })
+                .rollup(function(v) {
+                    return {
+                        make: v.map(function(d) {
+                            return d.Make;
+                        })[0],
+                        fatal: d3.sum(v, function(d) {
+                            return +d.Total_Fatal_Injuries;
+                        }),
+                        serious: d3.sum(v, function(d) {
+                            return +d.Total_Serious_Injuries;
+                        }),
+                        uninjured: d3.sum(v, function(d) {
+                            return +d.Total_Uninjured;
+                        }),
+                    };
+                })
                 .entries(data);
 
             var currentIncidents = incidents
                 .filter(function(d) {
-                    return d.key == "STANDING";
-                })[0].values.map(function(v) {
+                    return d.key == 'STANDING';
+                })[0]
+                .values.map(function(v) {
                     return v.value;
                 });
 
-            xScale.domain(currentIncidents.map(function(d) {
-                return d.make;
-            }));
+            xScale.domain(
+                currentIncidents.map(function(d) {
+                    return d.make;
+                })
+            );
 
-            yScale.domain([0, d3.max(incidents.filter(function(f) {
-                return f.key != "";
-            }).map(function(d) {
-                return d3.max(d.values.map(function(v) {
-                    return d3.max([v.value.fatal, v.value.serious, v.value.uninjured]);
-                }));
-            }))]);
+            yScale.domain([
+                0,
+                d3.max(
+                    incidents
+                    .filter(function(f) {
+                        return f.key != '';
+                    })
+                    .map(function(d) {
+                        return d3.max(
+                            d.values.map(function(v) {
+                                return d3.max([
+                                    v.value.fatal,
+                                    v.value.serious,
+                                    v.value.uninjured,
+                                ]);
+                            })
+                        );
+                    })
+                ),
+            ]);
 
-            bars.append('g')
+            bars
+                .append('g')
                 .attr('class', 'x axis')
                 .attr('transform', function(d) {
                     return 'translate(' + bigBuffer + ', ' + (height - bigBuffer) + ')';
                 })
                 .call(xAxis);
 
-            bars.append('g')
+            bars
+                .append('g')
                 .attr('class', 'y axis')
                 .attr('transform', function(d) {
                     return 'translate(' + bigBuffer + ', 0)';
                 })
                 .call(yAxis);
 
-            bars.append('g')
+            bars
+                .append('g')
                 .selectAll('.fatalBar')
                 .data(currentIncidents)
                 .enter()
                 .append('rect')
                 .attr('class', 'fatalBar')
                 .attr('x', function(d) {
-                    return bigBuffer + xScale(d.make) + (xScale.bandwidth() * .15);
+                    return bigBuffer + xScale(d.make) + xScale.bandwidth() * 0.15;
                 })
                 .attr('y', function(d) {
                     return yScale(d.fatal);
                 })
-                .attr('width', xScale.bandwidth() * .2)
+                .attr('width', xScale.bandwidth() * 0.2)
                 .attr('height', function(d) {
                     return height - yScale(d.fatal) - bigBuffer;
                 });
-            bars.append('g')
+            bars
+                .append('g')
                 .selectAll('.seriousBar')
                 .data(currentIncidents)
                 .enter()
                 .append('rect')
                 .attr('class', 'seriousBar')
                 .attr('x', function(d) {
-                    return bigBuffer + xScale(d.make) + (xScale.bandwidth() * .4);
+                    return bigBuffer + xScale(d.make) + xScale.bandwidth() * 0.4;
                 })
                 .attr('y', function(d) {
                     return yScale(d.serious);
                 })
-                .attr('width', xScale.bandwidth() * .2)
+                .attr('width', xScale.bandwidth() * 0.2)
                 .attr('height', function(d) {
                     return height - yScale(d.serious) - bigBuffer;
                 });
-                bars.append('g')
-                    .selectAll('.uninjuredBar')
-                    .data(currentIncidents)
-                    .enter()
-                    .append('rect')
-                    .attr('class', 'uninjuredBar')
-                    .attr('x', function(d) {
-                        return bigBuffer + xScale(d.make) + (xScale.bandwidth() * .65);
-                    })
-                    .attr('y', function(d) {
-                        return yScale(d.uninjured);
-                    })
-                    .attr('width', xScale.bandwidth() * .2)
-                    .attr('height', function(d) {
-                        return height - yScale(d.uninjured) - bigBuffer;
-                    });
+            bars
+                .append('g')
+                .selectAll('.uninjuredBar')
+                .data(currentIncidents)
+                .enter()
+                .append('rect')
+                .attr('class', 'uninjuredBar')
+                .attr('x', function(d) {
+                    return bigBuffer + xScale(d.make) + xScale.bandwidth() * 0.65;
+                })
+                .attr('y', function(d) {
+                    return yScale(d.uninjured);
+                })
+                .attr('width', xScale.bandwidth() * 0.2)
+                .attr('height', function(d) {
+                    return height - yScale(d.uninjured) - bigBuffer;
+                });
         }
     );
 }
