ccce8e161afff8a914d84d1dc000eba6ea8b7762
CalebBoerner <calebmboerner@gmail.com>
2019-04-21 17:18:33 -0400

    code cleanup and other minor text alterations. And I broke the buttons styles oops

--- main.js
+++ main.js
@@ -4,17 +4,7 @@ function start() {
     var allData;
 
     var standingGraph = document.getElementById('standingGraph');
-    var taxiGraph = document.getElementById('taxiGraph');
-    var takeoffGraph = document.getElementById('takeoffGraph');
-    var climbGraph = document.getElementById('climbGraph');
-    var cruiseGraph = document.getElementById('cruiseGraph');
-    var descentGraph = document.getElementById('descentGraph');
-    var approachGraph = document.getElementById('approachGraph');
-    var landingGraph = document.getElementById('landingGraph');
     var buttonBar = document.getElementById('buttonBar');
-
-    //console.log(graph);
-    console.log(buttonBar);
 
     var width = 900;
     var height = 400;
@@ -25,41 +15,6 @@ function start() {
 
     var standingSvg = d3
         .select(standingGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var taxiSvg = d3
-        .select(taxiGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var takeoffSvg = d3
-        .select(takeoffGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var climbSvg = d3
-        .select(climbGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var cruiseSvg = d3
-        .select(cruiseGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var descentSvg = d3
-        .select(descentGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var approachSvg = d3
-        .select(approachGraph)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-    var landingSvg = d3
-        .select(landingGraph)
         .append('svg')
         .attr('width', width)
         .attr('height', height);
@@ -95,15 +50,17 @@ function start() {
     d3.select(buttonBar)
         .append('button')
         .attr('id', 'leftButton')
+        .attr('class', 'btn btn-outline-warning')
         .attr('class', 'invisibleButton')
         .attr('transform', function(d) {
             return 'translate(' + bigBuffer + ', 0)';
         })
-        .text('Click Me!')
+        .text('')
         .on('click', button1);
     d3.select(buttonBar)
         .append('button')
         .attr('id', 'rightButton')
+        .attr('class', 'btn btn-outline-warning')
         .attr('class', 'visibleButton')
         .attr('transform', function(d) {
             return 'translate(' + (width - bigBuffer) + ', 0)';
@@ -144,9 +101,11 @@ function start() {
             leftVisibility = 'invisibleButton';
         }
         d3.select(document.getElementById('leftButton'))
+            .attr('class', 'btn btn-outline-warning')
             .attr('class', leftVisibility)
             .text(leftPhase);
         d3.select(document.getElementById('rightButton'))
+            .attr('class', 'btn btn-outline-warning')
             .attr('class', rightVisibility)
             .text(rightPhase);
         updateBars();
@@ -249,7 +208,7 @@ function start() {
             return 1000;
         })
         .delay(function(d) {
-            return Math.random() * 1500;
+            return Math.random() * 500 + 500;
         })
         .attr('y', function(d) {
             return yScale(d3.sum(d.value.map(function(d) {
@@ -285,16 +244,11 @@ function start() {
             });
 
         y.merge(newY).transition(t).call(yAxis);
+
+        document.getElementById("graphTitle").innerHTML = "Flight Phase: " + currentPhase;
     }
 
     var standingBars = standingSvg.append('g');
-    var taxiBars = taxiSvg.append('g');
-    var takeoffBars = takeoffSvg.append('g');
-    var climbBars = climbSvg.append('g');
-    var cruiseBars = cruiseSvg.append('g');
-    var descentBars = descentSvg.append('g');
-    var approachBars = approachSvg.append('g');
-    var landingBars = landingSvg.append('g');
 
     var xScale = d3.scaleBand().rangeRound([0, width - bigBuffer * 2], 0.3);
     var yScale = d3.scaleLinear().range([height - bigBuffer, littleBuffer]);
@@ -358,7 +312,6 @@ function start() {
                     return d.Make;
                 })
                 .sortKeys(d3.ascending)
-                //.key(function(d) { return d.Broad_Phase_of_Flight; })
                 .rollup(function(v) {
                     return v.map(function(d) {
                         return {
@@ -495,17 +448,9 @@ function start() {
                     }))]);
             }))]);
 
-            buildChart(standingBars, test);
-            //buildChart(standingBars, standingIncidents);
-            //buildChart(taxiBars, taxiIncidents);
-            //buildChart(takeoffBars, takeoffIncidents);
-            //buildChart(climbBars, climbIncidents);
-            //buildChart(cruiseBars, cruiseIncidents);
-            //buildChart(descentBars, descentIncidents);
-            //buildChart(approachBars, approachIncidents);
-            //buildChart(landingBars, landingIncidents);
+            buildChart(standingBars);
 
-            function buildChart(bars, incidentData) {
+            function buildChart(bars) {
                 bars.append('g')
                     .attr('class', 'xAxis')
                     .attr('transform', function(d) {
@@ -518,8 +463,7 @@ function start() {
                         return 'translate(' + bigBuffer + ', 0)';
                     })
                     .call(yAxis);
-                bars
-                    .append('g')
+                bars.append('g')
                     .selectAll('.fatalBar')
                     .data(test)
                     .enter()
@@ -539,8 +483,7 @@ function start() {
                             return d.fatal;
                         }))) - bigBuffer;
                     });
-                bars
-                    .append('g')
+                bars.append('g')
                     .selectAll('.seriousBar')
                     .data(test)
                     .enter()
@@ -560,88 +503,6 @@ function start() {
                             return d.serious;
                         }))) - bigBuffer;
                     });
-
-            // xScale.domain(standingIncidents.map(function(d) {
-            //     return d.make;
-            // }));
-            // yScale.domain([0, d3.max(incidents.filter(function(f) {
-            //     return f.key != "";
-            // }).map(function(d) {
-            //     return d3.max(d.values.map(function(v) {
-            //         return d3.max([v.value.fatal, v.value.serious]);//, v.value.uninjured]);
-            //     }));
-            // }))]);
-
-            // buildChart(standingBars, standingIncidents);
-            // buildChart(taxiBars, taxiIncidents);
-            // buildChart(takeoffBars, takeoffIncidents);
-            // buildChart(climbBars, climbIncidents);
-            // buildChart(cruiseBars, cruiseIncidents);
-            // buildChart(descentBars, descentIncidents);
-            // buildChart(approachBars, approachIncidents);
-            // buildChart(landingBars, landingIncidents);
-
-            // function buildChart(bars, incidentData) {
-            //     bars.append('g')
-            //         .attr('class', 'x axis')
-            //         .attr('transform', function(d) {
-            //             return 'translate(' + bigBuffer + ', ' + (height - bigBuffer) + ')';
-            //         })
-            //         .call(xAxis);
-            //     bars.append('g')
-            //         .attr('class', 'y axis')
-            //         .attr('transform', function(d) {
-            //             return 'translate(' + bigBuffer + ', 0)';
-            //         })
-            //         .call(yAxis);
-            //     bars.append('g')
-            //         .selectAll('.fatalBar')
-            //         .data(incidentData)
-            //         .enter()
-            //         .append('rect')
-            //         .attr('class', 'fatalBar')
-            //         .attr('x', function(d) {
-            //             return bigBuffer + xScale(d.make) + (xScale.bandwidth() * .15);
-            //         })
-            //         .attr('y', function(d) {
-            //             return yScale(d.fatal);
-            //         })
-            //         .attr('width', xScale.bandwidth() * .2)
-            //         .attr('height', function(d) {
-            //             return height - yScale(d.fatal) - bigBuffer;
-            //         });
-            //     bars.append('g')
-            //         .selectAll('.seriousBar')
-            //         .data(incidentData)
-            //         .enter()
-            //         .append('rect')
-            //         .attr('class', 'seriousBar')
-            //         .attr('x', function(d) {
-            //             return bigBuffer + xScale(d.make) + (xScale.bandwidth() * .4);
-            //         })
-            //         .attr('y', function(d) {
-            //             return yScale(d.serious);
-            //         })
-            //         .attr('width', xScale.bandwidth() * .2)
-            //         .attr('height', function(d) {
-            //             return height - yScale(d.serious) - bigBuffer;
-            //         });
-            //     // bars.append('g')
-            //     //     .selectAll('.uninjuredBar')
-            //     //     .data(incidentData)
-            //     //     .enter()
-            //     //     .append('rect')
-            //     //     .attr('class', 'uninjuredBar')
-            //     //     .attr('x', function(d) {
-            //     //         return bigBuffer + xScale(d.make) + (xScale.bandwidth() * .65);
-            //     //     })
-            //     //     .attr('y', function(d) {
-            //     //         return yScale(d.uninjured);
-            //     //     })
-            //     //     .attr('width', xScale.bandwidth() * .2)
-            //     //     .attr('height', function(d) {
-            //     //         return height - yScale(d.uninjured) - bigBuffer;
-            //     //     });
             }
         }
     );
